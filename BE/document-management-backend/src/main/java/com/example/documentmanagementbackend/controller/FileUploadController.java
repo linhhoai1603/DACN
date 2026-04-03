77f976fce89e64a90b51f79941cff2d7991cc482
Truong Quy Khoa <22130124@st.hcmuaf.edu.vn>
2026-04-03 10:53:30 +0700

    create api and logic for uploading file to Cloudinary

---
+++ BE/document-management-backend/src/main/java/com/example/documentmanagementbackend/controller/FileUploadController.java
@@ -0,0 +1,28 @@
+package com.example.documentmanagementbackend.controller;
+
+import com.example.documentmanagementbackend.dto.response.FileUploadResponse;
+import com.example.documentmanagementbackend.service.FileUploadService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/files")
+public class FileUploadController {
+    private final FileUploadService fileUploadService;
+
+    public FileUploadController(FileUploadService fileUploadService) {
+        this.fileUploadService = fileUploadService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<FileUploadResponse> uploadFile(
+            @RequestParam("file") MultipartFile file
+    ) {
+        FileUploadResponse response = fileUploadService.upload(file);
+        return ResponseEntity.ok(response);
+    }
+}
--- BE/document-management-backend/src/main/java/com/example/documentmanagementbackend/service/ExampleService.java
+++
@@ -1,4 +0,0 @@
-package com.example.documentmanagementbackend.service;
-
-public class ExampleService {
-}

37196030ba8cf773e9747d459970a97f71a6b28f
Truong Quy Khoa <22130124@st.hcmuaf.edu.vn>
2026-04-03 11:02:43 +0700

    add upload api path into Security Config

--- BE/document-management-backend/src/main/java/com/example/documentmanagementbackend/config/SecurityConfig.java
+++ BE/document-management-backend/src/main/java/com/example/documentmanagementbackend/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login").permitAll()
+                        .requestMatchers("/files/upload").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
 
--- BE/document-management-backend/src/main/java/com/example/documentmanagementbackend/controller/FileUploadController.java
+++ BE/document-management-backend/src/main/java/com/example/documentmanagementbackend/controller/FileUploadController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/files")
+@RequestMapping("/files")
 public class FileUploadController {
     private final FileUploadService fileUploadService;
 
